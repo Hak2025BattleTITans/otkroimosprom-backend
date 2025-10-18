0147b04279804f6f5ba47a4ec6c1e4f8772e4e91
Artem <wieredsh@gmail.com>
2025-10-18 12:59:35 +0300

    feat: use settings in main

--- src/main.py
+++ src/main.py
@@ -2,16 +2,14 @@
 import uvicorn
 
 from api.app import app
+from settings import settings
 
 if __name__ == "__main__":
-    host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", "2201"))
-    log_level = os.getenv("LOG_LEVEL", "info")
-
-    # Для разработки можно включить hot-reload через переменную окружения
-    reload_enabled = os.getenv("UVICORN_RELOAD", "false").lower() in ("1", "true", "yes")
-    # В проде можно скейлить воркеры (игнорируется, если включён reload)
-    workers = int(os.getenv("UVICORN_WORKERS", "1"))
+    host = settings.host
+    port = settings.port
+    log_level = settings.logging_level
+    reload_enabled = settings.reload in ("1", "true", "yes")
+    workers = settings.workers
 
     uvicorn.run(
         app,
