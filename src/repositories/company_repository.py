64d755dfef256f4ddb40441a786ab786f168780e
Artem <wieredsh@gmail.com>
2025-10-18 13:53:15 +0300

    rename(data_repository): rename data repository to company repository

--- src/repositories/company_repository.py
+++ src/repositories/company_repository.py
@@ -3,7 +3,7 @@ from typing import List
 
 from sqlmodel import Session, select
 
-from src.models import ConsolidatedData, ConsolidatedDataUpdate
+from src.models.models import CompanyCreate, CompanyUpdate, CompanyRead
 
 logger = logging.getLogger(__name__)
 
@@ -11,30 +11,38 @@ class DataRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    def create(self, data: ConsolidatedData) -> ConsolidatedData:
+    def create(self, data: CompanyCreate) -> CompanyCreate:
         self.session.add(data)
         self.session.commit()
         self.session.refresh(data)
         return data
 
-    def get_by_id(self, record_id: int) -> ConsolidatedData | None:
-        return self.session.get(ConsolidatedData, record_id)
+    def get_by_id(self, record_id: int) -> CompanyRead | None:
+        return self.session.get(CompanyRead, record_id)
 
-    def list_all(self, skip: int = 0, limit: int = 100) -> List[ConsolidatedData]:
-        statement = select(ConsolidatedData).offset(skip).limit(limit)
+    def list_all(self, skip: int = 0, limit: int = 100) -> List[CompanyRead]:
+        statement = select(CompanyRead).offset(skip).limit(limit)
         results = self.session.exec(statement).all()
         return results
 
-    def get_by_inn_and_year(self, inn: str, year: int) -> ConsolidatedData | None:
-        statement = select(ConsolidatedData).where(ConsolidatedData.inn == inn, ConsolidatedData.year == year)
+    def get_by_inn(self, inn: str) -> CompanyRead | None:
+        statement = select(CompanyRead).where(CompanyRead.inn == inn)
         return self.session.exec(statement).first()
 
-    def update(self, db_obj: ConsolidatedData, obj_in: ConsolidatedDataUpdate) -> ConsolidatedData:
+    def get_by_inn_and_year(self, inn: str, year: int) -> CompanyRead | None:
+        statement = select(CompanyRead).where(CompanyRead.inn == inn, CompanyRead.year == year)
+        return self.session.exec(statement).first()
+
+    def get_by_industry(self, industry: str) -> CompanyRead | None:
+        statement = select(CompanyRead).where(CompanyRead.industry == industry)
+        return self.session.exec(statement).first()
+
+    def update(self, db_obj: CompanyRead, obj_in: CompanyUpdate) -> CompanyRead:
         """Частично обновляет запись в БД."""
         update_data = obj_in.model_dump(exclude_unset=True)
         for key, value in update_data.items():
             setattr(db_obj, key, value)
-        
+
         self.session.add(db_obj)
         self.session.commit()
         self.session.refresh(db_obj)
@@ -45,7 +53,7 @@ class DataRepository:
         record = self.get_by_id(record_id)
         if not record:
             return False
-        
+
         self.session.delete(record)
         self.session.commit()
         return True
