4c9eec345a5c1be776b310ee3e31291750a498bc
Artem <wieredsh@gmail.com>
2025-10-18 13:57:45 +0300

    fix(api): fix app.py

--- src/api/app.py
+++ src/api/app.py
@@ -9,8 +9,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from api import auth_router
 from api.data_router import router as data_router
 from api.user_router import router as user_router
+from api.files import router as files_router
 from logging_config import LOGGING_CONFIG, ColoredFormatter
-from src.database import init_db 
+from src.database import init_db
 
 # Setup logging
 dictConfig(LOGGING_CONFIG)
@@ -26,6 +27,7 @@ app = FastAPI()
 # Set up API routers
 api_v1 = APIRouter(prefix="/v1", tags=["v1"])
 api_v1.include_router(auth_router)
+api_v1.include_router(files_router)
 
 app.include_router(api_v1, prefix="/api")
 
@@ -45,47 +47,6 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],            # разрешить все методы (GET, POST и т.д.)
     allow_headers=["*"],            # разрешить все заголовки
-)
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Код, который выполнится при старте приложения
-    logger.info("Application startup...")
-    logger.info("Initializing database...")
-    init_db() # Создаем таблицы в БД при старте
-    yield
-    # Код, который выполнится при завершении работы
-    logger.info("Application shutdown.")
-
-app = FastAPI(
-    title="OtkroiMosprom API",
-    description="API для проекта 'Откроймоспром'",
-    version="1.0.0",
-    lifespan=lifespan 
-)
-
-# Настройка API роутеров
-api_v1 = APIRouter(prefix="/v1")
-api_v1.include_router(auth_router)
-api_v1.include_router(data_router) 
-api_v1.include_router(user_router)
-app.include_router(api_v1, prefix="/api")
-
-# Настройка CORS
-origins = [
-    "https://ruzserver.ru",
-    "https://okto.ruzserver.ru",
-    "https://www.ruzserver.ru",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
 )
 
 @app.get("/")
